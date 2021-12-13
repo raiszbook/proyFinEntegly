90c219545c9cd400d469dc44003162737e2c70a2
raiszbook <raisz2012@gmail.com>
2021-12-13 00:26:43 -0500

    first commit

---
+++ entelgyv7/src/main/java/com/proy/entelgyv7/Entelgyv7Application.java
@@ -0,0 +1,13 @@
+package com.proy.entelgyv7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Entelgyv7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Entelgyv7Application.class, args);
+	}
+
+}
